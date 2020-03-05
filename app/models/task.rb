ede301f6a36812241394f1cda04f01b2e77c9f9e
塚崎成寛 <n-tsukazaki@ruby-dev.jp>
2020-03-05 15:44:30 +0900

    refs #14550 fix task.rb

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,4 @@
 class Task < ApplicationRecord
-    validates :name, length: {maximum: 20}, presence :true
-    validates :memo, length: {maximum: 100}
+  validates :name, length: { maximum: 20 }, presence: true
+  validates :memo, length: { maximum: 100 }
 end
