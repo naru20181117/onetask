01453b81b7d03f429801eee27557d809cd88ecab
塚崎成寛 <n-tsukazaki@ruby-dev.jp>
2020-03-05 14:25:08 +0900

    refs #14550 set validations

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,4 @@
 class Task < ApplicationRecord
+    validates :name, length: {maximum: 20}, presence :true
+    validates :memo, length: {maximum: 100}
 end
--- db/migrate/20200305044748_create_tasks.rb
+++ db/migrate/20200305044748_create_tasks.rb
@@ -2,7 +2,7 @@ class CreateTasks < ActiveRecord::Migration[6.0]
   def change
     create_table :tasks do |t|
       t.string :name, null: false, limit: 20
-      t.string :memo, null: false, limit: 50
+      t.string :memo, limit: 100
 
       t.timestamps
     end
