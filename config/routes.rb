23900dadb82ae69f9513c3be809e6c6c158c46cb
塚崎成寛 <n-tsukazaki@ruby-dev.jp>
2020-03-06 14:34:58 +0900

    refs #14591 rails serverを起動するためwebpackをインストール

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
+  root 'tasks#show'
   get 'tasks/new'
-  get 'tasks/show'
+  # get 'tasks/show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
