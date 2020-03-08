649a3ca00640f1be6a2adb6cdd74be1edc9ac930
塚崎成寛 <n-tsukazaki@ruby-dev.jp>
2020-03-09 08:37:12 +0900

    refs #14591 showのroutingを設定

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,18 @@
 # frozen_string_literal: true
 
 class TasksController < ApplicationController
+
   def new
+    @task = Task.new
   end
 
   def show
-    @task = Task.new
+    @task = Task.find(params[:id])
+    # render :text => "id =#{params[:id]}"
   end
+
+  def list
+    @tasks = Task.all
+  end
+
 end
