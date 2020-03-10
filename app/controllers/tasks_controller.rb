e88463a75afb032e62f1f2cb53ce3f2a7ea43ad8
塚崎成寛 <n-tsukazaki@ruby-dev.jp>
2020-03-10 11:43:23 +0900

    refs #消すやつ

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,15 +1,43 @@
 # frozen_string_literal: true
 
 class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
   def new
     @task = Task.new
+    @task.save
+  end
+
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      logger.debug "task: #{@task.attributes.inspect}"
+      flash[:success] = "もっとタスクを増やしていこう！"
+      redirect_to @task
+    else
+      render 'new'
+    end
   end
 
   def show
     @task = Task.find(params[:id])
   end
 
-  def index
-    @tasks = Task.all
+  def edit
+    @task = Task.find_by(id: params[:id])
+  end
+
+  def update
+    @task = Task.find_by(id: params[:id])
+    @task.update(task_params)
+    redirect_to :root
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:name, :memo)
   end
 end
