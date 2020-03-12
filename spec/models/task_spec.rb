ade8145e71fc70e035ddd8fcbc84c7c53a1fccbc
塚崎成寛 <n-tsukazaki@ruby-dev.jp>
2020-03-13 08:10:49 +0900

    refs #14873 model specを書く

---
+++ spec/models/task_spec.rb
@@ -0,0 +1,18 @@
+require './spec/rails_helper.rb'
+
+RSpec.describe Task do
+  describe 'set_task' do
+    it "is valid with a name, memo" do
+      @task = Task.new(
+        name: 'タスクの名前',
+        memo: 'Task1'
+      )
+      expect(@task).to be_valid
+    end
+
+    it "is invalid without a first_name" do
+      @task = Task.new(name: nil)
+      expect(@task.valid?).to eq(false)
+    end
+  end
+end
