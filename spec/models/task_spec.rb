fff50d9f2bd6313534d0c48ffa830b39bb3595da
塚崎成寛 <n-tsukazaki@ruby-dev.jp>
2020-03-16 17:20:59 +0900

    refs #14873 バリーデーションに対するテスト項目の追加/ 書き方の修正

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -3,17 +3,39 @@
 require './spec/rails_helper.rb'
 RSpec.describe Task do
   describe 'valid_task' do
-    let!(:task) { build :task }
-    it "is valid with a name, memo" do
-      expect(task).to be_valid
+    context "when the name is less than 20 letters" do
+      let(:task) { build :task, name: "#{'a' * 15}.to_s" }
+      it "is valid" do
+        expect(task).to be_valid
+      end
+    end
+    context "when the memo is less than 100 letters" do
+      let(:task) { build :task, memo: "#{'a' * 95}.to_s" }
+      it "is valid" do
+        expect(task).to be_valid
+      end
     end
   end
 
   describe 'invalid_task' do
-    let!(:task) { build :task, name: nil }
-    it "is invalid with a wrong name" do
-      expect(task).to be_invalid
-      expect(task.errors[:name]).to include("can't be blank")
+    context "when the name has more than 20 letters" do
+      let(:task) { build :task, name: "#{'a' * 16}.to_s" }
+      it "is in_valid" do
+        expect(task).to be_invalid
+      end
+    end
+    context 'when the name is nil' do
+      let(:task) { build :task, name: nil }
+      it "is invalid" do
+        expect(task).to be_invalid
+        expect(task.errors[:name]).to include("can't be blank")
+      end
+    end
+    context "when the memo has more than 100 letters" do
+      let(:task) { build :task, memo: "#{'a' * 96}.to_s" }
+      it "is valid" do
+        expect(task).to be_invalid
+      end
     end
   end
 end
--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -4,16 +4,18 @@ require 'rails_helper'
 RSpec.describe "Tasks", type: :system do
   describe 'crud' do
     describe '#index' do
-      it "enables me to see" do
-        visit tasks_path
-        click_link "One Task"
-        expect(page).to have_content("Tasks Table")
+      context 'click the home button' do
+        it "enables me to see home" do
+          visit new_task_path
+          click_link "One Task"
+          expect(page).to have_content("Tasks Table")
+        end
       end
     end
 
     describe '#new' do
-      context "enable" do
-        it "create new task" do
+      context "create new task with name" do
+        it "enable to create one" do
           visit new_task_path
           fill_in "Name", with: "My Task"
           click_button "Submit"
@@ -22,8 +24,8 @@ RSpec.describe "Tasks", type: :system do
           expect(page).to have_selector '.success', text: "もっとタスクを増やしていこう！"
         end
       end
-      context 'disable' do
-        it "create new task" do
+      context 'create new task with name nil' do
+        it "disable to create any" do
           visit new_task_path
           fill_in "Name", with: ""
           click_button "Submit"
@@ -34,34 +36,41 @@ RSpec.describe "Tasks", type: :system do
     end
 
     describe '#edit' do
-      let!(:task) { create :task }
-      it "enables me to edit task" do
-        visit edit_task_path(task)
-        fill_in "Name", with: "Edited_Task"
-        click_button "Submit"
-        expect(page).to have_content("Tasks Table")
-        expect(page).to have_content("Edited_Task")
-        expect(page).to have_selector '.success', text: "Task変更完了！"
+      let(:task) { create :task }
+      context 'edit name with valid words' do
+        it "enables me to edit task" do
+          visit edit_task_path(task)
+          fill_in "Name", with: "Edited_Task"
+          click_button "Submit"
+          expect(page).to have_content("Tasks Table")
+          expect(page).to have_content("Edited_Task")
+          expect(page).to have_selector '.success', text: "Task変更完了！"
+        end
       end
-
-      it "disables me to edit task" do
-        visit edit_task_path(task)
-        fill_in "Name", with: ""
-        click_button "Submit"
-        expect(page).to have_content("Update Your Task")
-        expect(page).to have_content("error")
-        expect(page).to have_selector '.alert', text: "Task変更失敗"
+      context 'edit name without name' do
+        it "disables me to edit task" do
+          visit edit_task_path(task)
+          fill_in "Name", with: ""
+          click_button "Submit"
+          expect(page).to have_content("Update Your Task")
+          expect(page).to have_content("error")
+          expect(page).to have_selector '.alert', text: "Task変更失敗"
+          expect(task.name).to have_content("hoge")
+        end
       end
     end
 
     describe '#destroy' do
-      let!(:task) { create :task }
-      it "enables me to destroy task" do
-        visit tasks_path
-        click_button "Delete"
-        page.driver.browser.switch_to.alert.accept
-        expect(page).not_to have_content("hoge")
-        expect(page).to have_selector '.notice', text: "Deleted the task"
+      context 'click the delete button' do
+        let!(:task) { create :task }
+        it "enables me to destroy task" do
+          visit tasks_path
+          click_button "Delete"
+          page.driver.browser.switch_to.alert.accept
+          expect(page).not_to have_content("hoge")
+          expect(page).to have_selector '.notice', text: "Deleted the task"
+          expect(Task.count).to eq 0
+        end
       end
     end
   end
