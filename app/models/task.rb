21aa6a56029b5820fddba85d9375ce2081570a44
塚崎成寛 <n-tsukazaki@ruby-dev.jp>
2020-03-16 11:31:35 +0900

    refs #14962 降順で並び替え

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class Task < ApplicationRecord
+  default_scope -> { order(created_at: :desc) }
   validates :name, length: { maximum: 20 }, presence: true
   validates :memo, length: { maximum: 100 }
 end
