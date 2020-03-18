ea0544ca71d62fc5a2d7b97a7ada1db7939963e3
塚崎成寛 <n-tsukazaki@ruby-dev.jp>
2020-03-18 18:05:43 +0900

    refs #14962 デフォルト設定の修正/記法修正

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 class Task < ApplicationRecord
-  default_scope -> { order(created_at: :desc) }
   validates :name, length: { maximum: 20 }, presence: true
   validates :memo, length: { maximum: 100 }
 end
--- config/initializers/time_formats.rb
+++ config/initializers/time_formats.rb
@@ -1,3 +1,3 @@
 # frozen_string_literal: true
 
-Time::DATE_FORMATS[:default] = '%Y%b%d %H:%M +9h'
+Time::DATE_FORMATS[:show] = '%Y%b%d %H:%M'
