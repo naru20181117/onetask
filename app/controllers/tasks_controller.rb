20f475aaf7ff254da9426385ad23d318af4a03a2
塚崎成寛 <n-tsukazaki@ruby-dev.jp>
2020-03-23 11:26:46 +0900

    refs #15077 ソート機能の実装

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,9 +2,42 @@
 
 class TasksController < ApplicationController
   before_action :set_task, only: %i(show edit update destroy)
+  before_action :set_variable, only: %i(index sort)
 
   def index
     @tasks = Task.order(created_at: :desc)
+  end
+
+  def sort
+    if params[:created_at].present?
+      @created_at_num = params[:created_at].to_i
+      if @created_at_num.zero?
+        @tasks = Task.order(created_at: :DESC)
+        @created_at = '登録日時▼'
+        @created_at_num = 1
+      else
+        @tasks = Task.order(created_at: :ASC)
+        @created_at = '登録日時▲'
+        @created_at_num = 0
+      end
+
+    elsif params[:end_time].present?
+      @end_time_num = params[:end_time].to_i
+
+      if @end_time_num.zero?
+        @tasks = Task.order(end_time: :DESC)
+        @end_time = '終了期限▼'
+        @end_time_num = 1
+      else
+        @tasks = Task.order(end_time: :ASC)
+        @end_time = '終了期限▲'
+        @end_time_num = 0
+      end
+
+    else
+      @tasks = Task.order(created_at: :desc)
+    end
+    render :index
   end
 
   def new
@@ -50,5 +83,12 @@ class TasksController < ApplicationController
 
   def set_task
     @task = Task.find(params[:id])
+  end
+
+  def set_variable
+    @created_at = '登録日時△'
+    @end_time = '終了期限△'
+    @created_at_num = 0
+    @end_time_num = 0
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   root 'tasks#index'
-  resources :tasks
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :tasks do
+    collection do
+      get :sort
+    end
+  end
 end
