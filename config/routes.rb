cd7ec2910aaa7006adf73dcbbe1424986cb72ebf
塚崎成寛 <n-tsukazaki@ruby-dev.jp>
2020-04-14 13:42:10 +0900

    refs #15695 フォームの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root 'tasks#index'
   resources :tasks
+  resources :users
   post '/tasks/:id/done' => 'tasks#done', as: 'done'
+  get '/login', to: 'sessions#new'
 end
