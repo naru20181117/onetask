82599717dd520ba6f50b68dfff04d473d57ae2ed
塚崎成寛 <n-tsukazaki@ruby-dev.jp>
2020-04-22 14:37:07 +0900

    refs #15696 adminでのUser機能実装

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,21 +1,19 @@
 # frozen_string_literal: true
 
 class Admin::UsersController < ApplicationController
+  before_action :set_user, only: %i(show edit update destroy)
+
   def index
     @users = User.all
   end
 
-  def show
-    @user = User.find(params[:id])
-  end
+  def show; end
 
   def new
     @user = User.new
   end
 
-  def edit
-    @user = User.find(params[:id])
-  end
+  def edit; end
 
   def create
     @user = User.new(user_params)
@@ -28,8 +26,6 @@ class Admin::UsersController < ApplicationController
   end
 
   def update
-    @user = User.find(params[:id])
-
     if @user.update(user_params)
       redirect_to admin_user_path(@user), notice: "ユーザー【#{@user.name}】を更新しました。"
     else
@@ -38,14 +34,17 @@ class Admin::UsersController < ApplicationController
   end
 
   def destroy
-    @user = User.find(params[:id])
     @user.destroy
-    redirect_to admin_users_url, notice: "ユーザー【#{@user.name}】を削除しました。"
+    redirect_to admin_users_path
   end
 
   private
 
   def user_params
     params.require(:user).permit(:name, :email, :admin, :password, :password_confirmation)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
