6aa132582806c0202056576dd1d0e0ce0a82a2d1
塚崎成寛 <n-tsukazaki@ruby-dev.jp>
2020-04-27 11:43:58 +0900

    refs #15698 Labelモデルを作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_20_232516) do
+ActiveRecord::Schema.define(version: 2020_04_27_024303) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "labels", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "tasks", force: :cascade do |t|
     t.string "name", limit: 20, null: false
