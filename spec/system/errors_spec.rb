ff0d7698a6ef2f466a87b040ea87fb2952b5eacb
塚崎成寛 <n-tsukazaki@ruby-dev.jp>
2020-05-05 21:33:57 +0900

    refs #15699 エラーページの表示Spec実装

---
+++ spec/system/errors_spec.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+RSpec.describe "Errors", js: true, type: :system do
+  describe '404' do
+    context 'when you get to none existed page' do
+      before { visit '/404test' }
+      it 'shows 404 page' do
+        expect(page).to have_content 'Page　Not　Found'
+      end
+    end
+  end
+  describe '500' do
+    context 'when you get internal server error' do
+      before do
+        allow_any_instance_of(SessionsController).to receive(:new).and_throw(Exception)
+        visit login_path
+      end
+      it 'shows 404 page' do
+        expect(page).to have_content 'Internal　Server　Error'
+      end
+    end
+  end
+end
