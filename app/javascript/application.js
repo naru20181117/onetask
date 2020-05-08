04cc1d731587cf6ec6270bb319355ff7cb7263a7
塚崎成寛 <n-tsukazaki@ruby-dev.jp>
2020-05-08 10:52:18 +0900

    refs #16299 jsでのhover色変え実装

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,1 +1,4 @@
 //= require rails-ujs
+//= require activestorage
+//= require turbolinks
+//= require_tree .
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,10 +3,12 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-require("@rails/ujs").start()
-// require("turbolinks").start()
-// require("@rails/activestorage").start()
-require("channels")
+require("@rails/ujs").start();
+// require("turbolinks").start();
+require("@rails/activestorage").start();
+require("channels");
+
+require("custom/task");
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -15,7 +17,7 @@ require("channels")
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 
-import Rails from 'rails-ujs'
-Rails.start()
+import Rails from "rails-ujs";
+Rails.start();
 
-import "../stylesheets/application"
+import "../stylesheets/application";
