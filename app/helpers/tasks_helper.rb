0420e664838e27c4891b2ca93d3cf86d574695bd
塚崎成寛 <n-tsukazaki@ruby-dev.jp>
2020-05-11 20:54:05 +0900

    refs #16299 statusによる色の変更

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -12,4 +12,29 @@ module TasksHelper
   def priority_sub
     params[:sort_column] == "priority" ? '優先度▼' : '優先度△'
   end
+
+  def end_time_color(task)
+    time = task.end_time
+    if task.status == 'done'
+      "bg-secondary"
+    elsif time <= Time.zone.today + 3.days || (time <= Time.zone.today + 5.days && task.priority == "high")
+      "bg-danger"
+    elsif time <= Time.zone.today + 5.days || (time <= Time.zone.today + 8.days && task.priority == "high")
+      "bg-warning"
+    elsif task.status == 'wip'
+      "bg-info"
+    end
+  end
+
+  def hidden_fire(task)
+    if task.status == 'done' || task.end_time > Time.zone.today + 5.days
+      "d-none"
+    end
+  end
+
+  def hidden_primary(task)
+    if task.status == 'done' || task.priority != 'high'
+      "d-none"
+    end
+  end
 end
