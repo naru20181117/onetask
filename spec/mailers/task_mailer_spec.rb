566f3bf5ee4d1868941ad204901fc149712b5666
塚崎成寛 <n-tsukazaki@ruby-dev.jp>
2020-06-04 20:09:37 +0900

    メール送信Spec作成

--- spec/mailers/task_mailer_spec.rb
+++ spec/mailers/task_mailer_spec.rb
@@ -1,5 +1,23 @@
-require "rails_helper"
+# frozen_string_literal: true
 
+require "rails_helper"
 RSpec.describe TaskMailer, type: :mailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user_a) { create :user }
+  let(:task) { create :task, name: 'メイラー', user: user_a }
+
+  let(:html_body) do
+    mail.body.raw_source
+  end
+
+  describe '#creation_email' do
+    let(:mail){ TaskMailer.creation_email(task) }
+
+    it 'can send email correctly' do
+      expect(mail.subject).to eq('タスク作成完了メール')
+      expect(mail.to).to eq(['user@example.com'])
+      expect(mail.from).to eq(['taskleaf@example.com'])
+
+      expect(html_body).to match('以下のタスクを作成しました')
+    end
+  end
 end
