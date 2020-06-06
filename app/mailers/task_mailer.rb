cfdfdafdb6008fa2699aa0e08eeae2bfd760825f
塚崎成寛 <n-tsukazaki@ruby-dev.jp>
2020-06-06 14:37:12 +0900

    set: sidekiq to send email

--- app/mailers/task_mailer.rb
+++ app/mailers/task_mailer.rb
@@ -1,11 +1,12 @@
 class TaskMailer < ApplicationMailer
   default from: 'taskleaf@example.com'
 
-  def creation_email(task)
+  def creation_email(task, current_user)
     @task = task
+    @current_user = current_user
     mail(
-      subject: 'タスク作成完了メール',
-      to: 'user@example.com',
+      subject: 'Taskリマインドメール',
+      to: current_user.email,
       from: 'taskleaf@example.com'
     )
   end
