cdd0a6b95165ac469a67bac9b36adcbdf9d7a24f
塚崎成寛 <n-tsukazaki@ruby-dev.jp>
2020-10-28 12:18:36 +0900

    set: routes simulater

--- app/javascript/custom/hello.js
+++ app/javascript/custom/hello.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
-    React.createElement('div', null, 'Help World'),
+    React.createElement('div', null, ''),
     document.body.appendChild(document.createElement('div')),
     );
 });
